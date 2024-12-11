e56e5d488fe52ca792c805eba176c265d18fa34a
Satya Santosh <145853199+SatyaSantosh197@users.noreply.github.com>
2024-12-11 21:37:18 +0530

    removed log collection

--- backend/routes/logRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const logController = require('../controllers/logController');
-const router = express.Router();
-
-router.get('/', logController.getLogs);
-
-module.exports = router;
